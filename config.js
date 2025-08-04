464749aadfd7c4f6302a0b87541bebb70e37527c
aphukong <aphukong@gmail.com>
2025-08-04 16:36:22 +0700

    config1

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config();
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: process.env.DB_HOST || "203.113.123.194",
+    host: process.env.DB_HOST || "203.113.123.197",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "rootpassword",
